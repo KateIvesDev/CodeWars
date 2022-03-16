6b7c81a96027b8efb65c2b497365c5bb3d30412d
Kate Ives <katie.ives30@gmail.com>
2022-03-16 11:20:07 -0500

    Update 03162022.js

--- 8kyu/03162022.js
+++ 8kyu/03162022.js
@@ -1,5 +1,5 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function even_or_odd(number) {
-    return number % 2 == 0 ? "Even" : "Odd";
+    return number % 2 == 0 ? "Even" : "Odd"; 
  }
