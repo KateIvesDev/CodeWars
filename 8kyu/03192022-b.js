ad331e511026fb77ce06f5d52db9e74c8730c0cf
Kate Ives <katie.ives30@gmail.com>
2022-03-19 20:10:37 -0500

    add kata

---
+++ 8kyu/03192022-b.js
@@ -0,0 +1,5 @@
+/*Make a simple function called greet that returns the most-famous "hello world!". Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?*/
+
+function greet(){
+    return String.fromCodePoint(104,101,108,108,111,32,119,111,114,108,100,33);
+  }
