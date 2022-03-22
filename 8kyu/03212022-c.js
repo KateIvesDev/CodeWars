301fad7ae9a951ad2c90be08be1e87fb570eb17f
Kate Ives <katie.ives30@gmail.com>
2022-03-21 19:01:12 -0500

    add kata

---
+++ 8kyu/03212022-c.js
@@ -0,0 +1,17 @@
+/*Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+0 => [0] */
+
+
+function digitize(n) {
+  let strNum = n.toString()
+  let newArray = strNum.split('');
+  let mapArray = newArray.map(x => parseInt(x))
+  
+  return mapArray.reverse();
+  
+}
+console.log(digitize(35231))
