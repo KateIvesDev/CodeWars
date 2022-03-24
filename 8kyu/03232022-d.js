908f5b0a459e3ff987b1f86ca8bd2e3ab56268ee
Kate Ives <katie.ives30@gmail.com>
2022-03-23 22:14:38 -0500

    add kata

---
+++ 8kyu/03232022-d.js
@@ -0,0 +1,6 @@
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.Return the average of the given array rounded down to its nearest integer.The array will never be empty.
+
+function getAverage(marks){
+    return Math.floor((marks.reduce((a,b) => a+b, 0)) / marks.length)
+   }
+console.log(getAverage([99, 98, 99, 97]))
