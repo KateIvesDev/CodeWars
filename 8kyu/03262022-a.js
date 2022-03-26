7c808d2b63e4a015944b61be021fdc118b18f5f2
Kate Ives <katie.ives30@gmail.com>
2022-03-26 08:51:58 -0500

    add kata
    Fundamentals, strings

---
+++ 8kyu/03262022-a.js
@@ -0,0 +1,17 @@
+/* Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+ */
+
+function DNAtoRNA(dna) {
+    dna = dna.toUpperCase();
+    return dna.replace(/T/g, "U")
+  // create a function which returns an RNA sequence from the given DNA sequence
+}
+console.log(DNAtoRNA("GCTATTA"))
