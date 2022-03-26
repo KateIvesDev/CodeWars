96372e31a48bf0535330f015885079ea5e0732f4
Kate Ives <katie.ives30@gmail.com>
2022-03-26 16:54:08 -0500

    add kata

---
+++ 8kyu/03262022-b.js
@@ -0,0 +1,22 @@
+/*You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.*/
+
+function check(a,x){
+    return(a.includes(x))?true:false;
+  };
+
+/*
+could also have done: 
+
+function check(a,x){
+  return a.includes(x);
+};
+
+OR
+
+const check = (a,x) => a.includes(x);
+
+*/
