299afcf788cc5040e9a1eaa2eb710c24c93c4291
Kate Ives <katie.ives30@gmail.com>
2022-03-31 21:34:32 -0500

    add kata

---
+++ 8kyu/03312022-b.js
@@ -0,0 +1,15 @@
+//Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+function countSheep(num){
+    let arr = []
+    if(num === 0) {
+      return ""
+    } else {
+      for (let i = 1; i <= num; i++){
+         arr.push(`${i} sheep...`)
+      }
+        return arr.join("")
+    }
+  }
+  
+  (countSheep(2));
