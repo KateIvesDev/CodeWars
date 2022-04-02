e587a1fa47a06c73813eb3710a2c69d549b7def0
Kate Ives <katie.ives30@gmail.com>
2022-04-02 10:52:10 -0500

    add kata

---
+++ 8kyu/04022022-e.js
@@ -0,0 +1,5 @@
+/*our task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.*/
+
+function isDivideBy(number, a, b) {
+    return number % a === 0 && number  % b === 0 ? true : false;
+  }
