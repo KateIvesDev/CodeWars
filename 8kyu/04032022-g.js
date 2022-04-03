8d4fcdfdee7b518f45e7701b681e8c5aa609ff2d
Kate Ives <katie.ives30@gmail.com>
2022-04-02 22:29:39 -0400

    add kata

---
+++ 8kyu/04032022-g.js
@@ -0,0 +1,37 @@
+// The function is not returning the correct values. Can you figure out why?
+
+
+//missing "break"
+
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+        break
+      case 2:
+        name = 'Venus'
+        break
+      case 3:
+        name = 'Earth'
+        break
+      case 4:
+        name = 'Mars'
+        break
+      case 5:
+        name = 'Jupiter'
+        break
+      case 6:
+        name = 'Saturn'
+        break
+      case 7:
+        name = 'Uranus'
+        break
+      case 8:
+        name = 'Neptune'
+        break
+    }
+    
+    return name;
+  }
+  console.log(getPlanetName(2))
