aed39b7b9a2553702454544255bb4857084d7144
Kate Ives <katie.ives30@gmail.com>
2022-04-03 23:05:02 -0400

    add kata

---
+++ 8kyu/04042022-m.js
@@ -0,0 +1,8 @@
+/*Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions*/
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
