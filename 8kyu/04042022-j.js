cfd3e982c0f6dc41018cbef35bbb2ad4ae7d339f
Kate Ives <katie.ives30@gmail.com>
2022-04-03 22:53:15 -0400

    add kata

---
+++ 8kyu/04042022-j.js
@@ -0,0 +1,7 @@
+/*This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?*/
+
+//It was because they used == instead of = for assignment
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
