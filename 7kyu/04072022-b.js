d2ede2a68a13e8d24826e402d3e63a37cfd4218e
Kate Ives <katie.ives30@gmail.com>
2022-04-08 00:05:54 -0400

    add kata

---
+++ 7kyu/04072022-b.js
@@ -0,0 +1,10 @@
+/*Description:
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer*/
+
+function squareDigits(num){
+    return parseInt(num.toString().split("").map(value => value**2).join(""))
+}
