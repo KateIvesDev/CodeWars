333828ad0012e16ac6bf1fc07b1fb38cad64e6c3
Kate Ives <katie.ives30@gmail.com>
2022-04-09 13:02:01 -0400

    add kata

---
+++ 6kyu/04092022-a.js
@@ -0,0 +1,5 @@
+/*Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior" */
