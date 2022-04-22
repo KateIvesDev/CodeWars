bd1d70aae8103fd8c9621295e1688042caa1bac1
Kate Ives <katie.ives30@gmail.com>
2022-04-22 16:06:26 -0500

    add kata

---
+++ 8kyu/04222022-a.js
@@ -0,0 +1,21 @@
+/*Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+Example
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+The input array will always contain only positive numbers, and will never be empty or null. */
+
+function squareOrSquareRoot(array) {
+    array = array.map((value) => {
+        if (Number.isInteger(Math.sqrt(value)) === true) {
+            return Math.sqrt(value)
+        } else if (Number.isInteger(Math.sqrt(value)) === false) {
+            return value**2
+        }
+    })
+    return array
+  }
