6f5fca8dc153d0e3b6e89e47f076c23d9e0c08b9
Kate Ives <katie.ives30@gmail.com>
2022-05-02 19:29:39 -0500

    add kata

---
+++ 6kyu/05022022-b.js
@@ -0,0 +1,13 @@
+/*Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+*/
+
+function countBits(n) {
+  
+    let binary = n.toString(2).split('')
+    let result = binary.map(value => parseInt(value))
+    return result.reduce((a,b) => a+b)
+   
+  }
