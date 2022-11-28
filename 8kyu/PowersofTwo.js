6a0fcbaeed0fad0eddd1f08a4f3e1e9b1a641d17
Kate Ives <katie.ives30@gmail.com>
2022-11-28 10:15:37 -0600

    refactor and add PREP explanation

--- 8kyu/PowersofTwo.js
+++ 8kyu/PowersofTwo.js
@@ -8,3 +8,17 @@ function powersOfTwo(n){
     return list;
 }
 powersOfTwo(1);
+
+  //REFACTOR 
+  //Parameter given is a non-negative integer
+  //Return expected is an array of all the powers of 2 up to and including the parameter
+  //Examples, parameter of 1 would return [2^1], parameter of 4 would return [2^1, 2^2, 2^3, 2^4]
+  //Pseudocode: Create an empty array, use a "for loop" to increment the exponent and the array push() method to fill the empty array with the result of 2 to the power of each number between 0 and the parameter.
+  
+ function powersOfTwo1(n){
+  let arr = []
+  for (let i = 0; i <=n; i++){
+    arr.push(2**i)
+  }
+ }
+ powersOfTwo1(4)
