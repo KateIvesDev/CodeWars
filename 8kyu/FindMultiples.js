651d72c96d6b6b1fbb651240684511c3466ffb47
Kate Ives <katie.ives30@gmail.com>
2022-11-29 08:53:55 -0600

    refactor solution and add PREP explanation

--- 8kyu/FindMultiples.js
+++ 8kyu/FindMultiples.js
@@ -5,6 +5,12 @@ For example, if the parameters passed are (2, 6), the function should return [2,
 If you can, try writing it in only one line of code.*/
 
 
+//PREP
+//Parameters are positive integers, not including 0; first value is an integer (base), second value is a limit that will always be greater than the base
+//Return should be an array that includes multiples of the base integer up to/including the limit integer
+//Example: if argument is (3,12) return [3,6,9,12]
+//Pseudcode: create an empty array; use a for loop where i is equal to the base integer and the loop increments by one up to the limit integer; for every increment of the loop, if i can be divided by the base integer without a remainder (using modul), then i should be pushed to the empty array. once the loop is complete, return the array.
+
 
 function findMultiples(integer, limit) {
     let result=[]
@@ -15,3 +21,19 @@ function findMultiples(integer, limit) {
     }
     return result
   }
+
+  findMultiples(2,6)  
+
+  //REFACTOR
+  //determine the length of the return array by dividing the limit by the base and use the Math.floor(method) to get a whole number
+  // use Array.from() method to create an array of a sequence of numbers from 1 to the length  
+  //use the .map() method to multiply each number in the array by the base integer
+
+  /* 
+  function findMultiples(integer, limit){
+    let arrLength = Math.floor(limit/integer)
+    let newArr = Array.from({length: arrLength}, (v,i) => i+1)
+    return newArr.map(item => item * integer)
+  }
+  
+  */
