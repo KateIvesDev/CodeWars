bc8cbc7954a915617deffb07e3b519eaa733df99
Kate Ives <katie.ives30@gmail.com>
2022-12-24 19:07:19 -0600

    add kata

---
+++ 8kyu/AppleTurnover.js
@@ -0,0 +1,6 @@
+/*Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.*/
+
+function apple(x){
+    return x ** 2 > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox.`;
+  }
+apple(1000)
